66cd466bbdaa5d909900982ee79fdc5b8cd01fbb
NavreetSran <navreet.sf2001@gmail.com>
2025-09-25 14:33:53 -0700

    New topics

--- list_topics.py
+++ list_topics.py
@@ -2,64 +2,162 @@
 def list_basics():
     l=[1,2,3,4,5]
     print(l,type(l))
-    print(l[2])#This gives the element present on that index
+    print(l[2])#This gives the element present on that index.
 
 def list_indexing():
-    l=[1,2,3,[4,5,6]] #This is a nested list
+    l=[1,2,3,[4,5,6]] #This is a nested list.
     print(l[2])
-    print(l[3][2])#This gets the number present in nested list
+    print(l[3][2])#This gets the number present in nested list.
 
 def list_slicing(): 
     l=[2,3,"Hello",[5,6,7]]
     print(l[0:2]) #Will stop one index before listed argument(2).
     #if we want more than just defined value.
     print(l[1:]) # This will give everything after the listed index.
-    #Using SCI(Start, Condition(endpoint), Incriment)
+    #Using SCI(Start, Condition(endpoint), Incriment).
     print(l[0::2])
     #Reversing 
     print(l[-1::-1])
 
 def list_iteration():
     l=[10,20,30,40,50,60]
-    #Using length function and range function
+    #Using length function and range function.
     t=len(l)
     for n in range(t):
-        print(l[n])#n id treated as index number for the list to run
+        print(l[n])#n id treated as index number for the list to run.
 
     #Using list directly
     for a in l: #This method cannot be used to reverse it.
         print(a) #This sends list elemants to a and a gets printed.
 
-    #Reverseing the list
+    #Reversing the list.
     for n in range(t-1,-1,-1):
         print(l[n])
 
 def list_comprehension():
-    l=[] #this is made by using regular formulas we know
+    l=[] #this is made by using regular formulas we know.
     for a in range(1,11):
         l.append(a)
     
     print(l)
-#This is a list comprehension 
+#This is a list comprehension.
     n=[m for m in range(1,11)]
     print(n)
 
-# This can also be done with conditional statements
+# This can also be done with conditional statements.
     s=[h for h in range(1,11) if h%2==0]
     print(s)
 
-def list_function():
+def list_function1():
     l=[20,30,40,50,60,70,80]
     #This takes index  
-    del l[1] #This does not give you the element it deleted
-    # This takes index
-    print(l.pop(2))# pop does give you the element it deleted when you print it.
-    
+    del l[1] #This does not give you the element it deleted.
     print(l)
 
+    # This takes index
+    print(l.pop(2))# pop does give you the element it deleted when you print it.
+    print(l)
 
+    n=[20,30,40,50,60,70]
+    n.remove(60)
+    print(n) #This takes the element we want to remove from the list not the index number.
 
+    n.clear()
+    print(n) #This clear's out the list.
 
+def list_function2():
+    l=[1,2,3,4,5,6]
+
+    #Insert is used to insert new values in the list.
+    l.insert(0,8) #0 is the index and 8 is the value we want to insert.
+    print(l)
+    
+    #Append is used to insert the values in the list but without the index, so it will only insert at the end.
+    l.append(7)
+    print(l)
+    n=[5,7,8,9]
+    l.append(n)
+    print(l) #This puts the exact data type into the other variable.
+
+    #Extend is used to put the value from one variable to other.
+    l.extend(n)
+    print(l)
+
+def list_functions3():
+    l=[10,20,30,10,10,50]
+    # This helps count the recoccurance of a specific element in the list.
+    c=l.count(10)
+    print(c)
+
+    #This helps find the max value in the list.(For alphabets it goes sequence wise).
+    m=max(l)
+    print(m)
+
+    a=["Hello","World"]
+    m1=max(a)
+    print(m1)
+
+    #This helpls find the min value in the list.
+    m2=min(l)
+    m3=min(a)
+
+    print(m2)
+    print(m3)
+
+    #This is used to sort the list(by default is does ascending).
+    l.sort()
+    print(l)
+
+    #This reverses the list completely.
+    l.reverse()
+    print(l)
+
+    #This is used to know the index of the value by giving the valur to the function.
+    i=l.index(50)
+    print(i)   
+
+def zip_function():
+    #This is used to iterate two or more than 2 lists.
+    l=[10,20,30,40]
+    l1=[1,2,3,4]
+
+    #1st way
+    for a,b in zip(l,l1):
+        print(a,b)
+
+    #2nd way
+    #This is the logical way to iterate two lists simulataneously using range function.
+    t=len(l)
+    #This uses length of one of the lists to iterate both of them.
+    for h in range(t):
+        print(l[h],l1[h])
+
+#Fun fact: if one of them is longer than the other use the shorter one bacause only equal number of item gets iterated.
+# So in longer list the remainaing items gets ignored. or you will get error in the range function for h.
+
+def string_to_list():
+    # In this we get input from the user as a string and then we convert it to a list.
+    n=input("Enter The Value ")
+    print(n)
+
+    l=n.split()
+    print(l) # This was for single input.
+
+    #Now lets do for multiple inputs by the users.
+    l=[]
+    for a in range(3):
+        n=input("Enter the Value " + str(a) + ":-")
+        l.append(n) #This method works for single word in input not multiple as multiple words will go as a single elememt in the list.
+
+    print(l)
+
+    #Now lets do for multiple chatacter in single input with three inputs.
+    L=[]
+    for a in range(3):
+        n=input("Enter the value " + str(a) + ":-")
+        L.extend(n.split())
+
+    print(L)
 
 
 
@@ -71,8 +169,12 @@ if __name__ == "__main__":
     #list_indexing()
     #list_slicing()
     #list_iteration()
-    #ist_comprehension()
-    list_function()
+    #list_comprehension()
+    #list_function1()
+    #list_function2()
+    #list_functions3()
+    #zip_function()
+    string_to_list()
     pass
 
 
